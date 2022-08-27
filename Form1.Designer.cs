12936bfd9fa474f3505fc88dc50a1d5639cd93f1
CleitonPaes <106140045+CleitonPaes@users.noreply.github.com>
2022-08-26 22:54:29 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PortTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelPort = new System.Windows.Forms.Label();
+            this.LabelProtocol = new System.Windows.Forms.Label();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.Protocol = new System.Windows.Forms.ComboBox();
+            this.Test = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.TCPServer = new System.ComponentModel.BackgroundWorker();
+            this.UDPServer = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // LabelPort
+            // 
+            this.LabelPort.AutoSize = true;
+            this.LabelPort.Location = new System.Drawing.Point(69, 32);
+            this.LabelPort.Name = "LabelPort";
+            this.LabelPort.Size = new System.Drawing.Size(26, 13);
+            this.LabelPort.TabIndex = 0;
+            this.LabelPort.Text = "Port";
+            // 
+            // LabelProtocol
+            // 
+            this.LabelProtocol.AutoSize = true;
+            this.LabelProtocol.Location = new System.Drawing.Point(202, 32);
+            this.LabelProtocol.Name = "LabelProtocol";
+            this.LabelProtocol.Size = new System.Drawing.Size(46, 13);
+            this.LabelProtocol.TabIndex = 1;
+            this.LabelProtocol.Text = "Protocol";
+            // 
+            // Port
+            // 
+            this.Port.Location = new System.Drawing.Point(35, 48);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(100, 20);
+            this.Port.TabIndex = 2;
+            this.Port.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Protocol
+            // 
+            this.Protocol.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Protocol.FormattingEnabled = true;
+            this.Protocol.Items.AddRange(new object[] {
+            "TCP",
+            "UDP"});
+            this.Protocol.Location = new System.Drawing.Point(174, 47);
+            this.Protocol.Name = "Protocol";
+            this.Protocol.Size = new System.Drawing.Size(104, 21);
+            this.Protocol.TabIndex = 3;
+            // 
+            // Test
+            // 
+            this.Test.Location = new System.Drawing.Point(116, 128);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(75, 23);
+            this.Test.TabIndex = 4;
+            this.Test.Text = "Test";
+            this.Test.UseVisualStyleBackColor = true;
+            this.Test.Click += new System.EventHandler(this.Test_Click);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(134, 94);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(37, 13);
+            this.Status.TabIndex = 5;
+            this.Status.Text = "Status";
+            // 
+            // TCPServer
+            // 
+            this.TCPServer.WorkerSupportsCancellation = true;
+            this.TCPServer.DoWork += new System.ComponentModel.DoWorkEventHandler(this.TCPServer_DoWork);
+            this.TCPServer.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.TCPServer_RunWorkerCompleted);
+            // 
+            // UDPServer
+            // 
+            this.UDPServer.WorkerSupportsCancellation = true;
+            this.UDPServer.DoWork += new System.ComponentModel.DoWorkEventHandler(this.UDPServer_DoWork);
+            this.UDPServer.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.UDPServer_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 178);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Test);
+            this.Controls.Add(this.Protocol);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.LabelProtocol);
+            this.Controls.Add(this.LabelPort);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PortTester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelPort;
+        private System.Windows.Forms.Label LabelProtocol;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.ComboBox Protocol;
+        private System.Windows.Forms.Button Test;
+        private System.Windows.Forms.Label Status;
+        private System.ComponentModel.BackgroundWorker TCPServer;
+        private System.ComponentModel.BackgroundWorker UDPServer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text;
+
+namespace PortTester
+{
+    public partial class Form1 : Form
+    {
+        private bool Loading = false;
+        private int PortNumber = 0;
+        private string MYIP = "";
+        private bool UDPResult = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Protocol.Text = "TCP";
+        }
+
+        private void Test_Click(object sender, EventArgs e)
+        {
+            if (Loading == false && Port.TextLength >= 1 && Convert.ToInt32(Port.Text) >= 1 && Convert.ToInt32(Port.Text) <= 65535)
+            {
+                Loading = true;
+                PortNumber = Convert.ToInt32(Port.Text);
+                Test.Enabled = false;
+                Status.Text = " Wait";
+
+                Application.DoEvents();
+
+                if (MYIP == "")
+                {
+                    try
+                    {
+                        WebRequest request = WebRequest.Create("https://api.ipify.org");
+                        using WebResponse response = request.GetResponse();
+                        using StreamReader stream = new(response.GetResponseStream());
+                        {
+                            MYIP = stream.ReadToEnd();
+                        }
+                    }
+                    catch
+                    {
+                        Thread.Sleep(1000);
+                        Test.Enabled = true;
+
+                        Status.Text = "Verify the connection";
+
+                        Loading = false;
+                        return;
+                    }
+                }
+
+                if (Protocol.Text == "TCP")
+                {
+                    TCPServer.RunWorkerAsync();
+
+                    Thread.Sleep(1000);
+
+                    using TcpClient tcpClient = new();
+                    {
+                        IAsyncResult result = tcpClient.BeginConnect(MYIP, PortNumber, null, null);
+                        WaitHandle timeoutHandler = result.AsyncWaitHandle;
+                        try
+                        {
+                            if (!result.AsyncWaitHandle.WaitOne(1000, false))
+                            {
+                                tcpClient.Close();
+                                Status.Text = "Closed";
+                            }
+
+                            tcpClient.EndConnect(result);
+                            Status.Text = " Open";
+                        }
+                        catch (Exception)
+                        {
+                            tcpClient.Close();
+                            Status.Text = "Closed";
+                        }
+                        finally
+                        {
+                            timeoutHandler.Close();
+                        }
+                    }
+                }
+
+                if (Protocol.Text == "UDP")
+                {
+                    UDPServer.RunWorkerAsync();
+
+                    Thread.Sleep(1000);
+
+                    using UdpClient udpClient = new();
+                    {
+
+                        UDPResult = false;
+
+                        try
+                        {
+                            udpClient.Client.ReceiveTimeout = 1000;
+
+                            udpClient.Connect(MYIP, PortNumber);
+
+                            byte[] sendBytes = new byte[1];
+                            new Random().NextBytes(sendBytes);
+                            udpClient.Send(sendBytes, sendBytes.Length);
+
+                            IPEndPoint remoteIpEndPoint = new(IPAddress.Any, 0);
+                            byte[] result = udpClient.Receive(ref remoteIpEndPoint);
+
+                            if (result != null)
+                            {
+                                UDPResult = true;
+                            }
+                            udpClient.Close();
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+            }
+        }
+
+        private void TCPServer_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            IPAddress ipAddress = IPAddress.Any;
+            Socket listenSocket = new(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new(ipAddress, PortNumber);
+
+            listenSocket.Bind(localEndPoint);
+            listenSocket.Listen(1);
+            SocketAsyncEventArgs ex = new();
+            ex.Completed += AcceptCallback;
+            if (!listenSocket.AcceptAsync(ex))
+            {
+                AcceptCallback(listenSocket, ex);
+            }
+
+            static void AcceptCallback(object sender, SocketAsyncEventArgs e)
+            {
+                Socket listenSocket = (Socket)sender;
+                do
+                {
+                    try
+                    {
+                        Socket newSocket = e.AcceptSocket;
+                        Debug.Assert(newSocket != null);
+                        newSocket.Send(Encoding.ASCII.GetBytes("Hello socket!"));
+                        newSocket.Disconnect(false);
+                        newSocket.Close();
+                    }
+                    catch
+                    {
+                    }
+                    finally
+                    {
+                        e.AcceptSocket = null;
+                    }
+                } while (!listenSocket.AcceptAsync(e));
+            }
+        }
+
+        private void UDPServer_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            UdpClient udpServer = new(PortNumber);
+
+            udpServer.Client.ReceiveTimeout = 2000;
+
+            while (true)
+            {
+                var remoteEP = new IPEndPoint(IPAddress.Any, PortNumber);
+                _ = udpServer.Receive(ref remoteEP);
+                udpServer.Send(new byte[] { 1 }, 1, remoteEP);
+                udpServer.Close();
+            }
+        }
+
+        private void TCPServer_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            Loading = false;
+            Test.Enabled = true;
+        }
+
+        private void UDPServer_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            Loading = false;
+            Test.Enabled = true;
+
+            if (UDPResult == true)
+            {
+                Status.Text = " Open";
+            }
+            else
+            {
+                Status.Text = "Closed";
+            }
+        }
+    }
+}
